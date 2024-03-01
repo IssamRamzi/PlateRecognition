1f385322768d6c334b70b3e10e708a72b903dfd1
Ramzi <iss.benahmed@outlook.com>
2024-03-01 17:38:04 +0100

    Did some changes in the db and the controllers

--- src/main/java/com/kefelle/platerecognition/MenuController.java
+++ src/main/java/com/kefelle/platerecognition/MenuController.java
@@ -33,9 +33,7 @@ public class MenuController {
     @FXML
     VBox vBox = new VBox();
 
-    @FXML
     public ScrollPane scrollPaneManual = new ScrollPane();
-    @FXML
     VBox vBoxManual = new VBox();
 
     /*
@@ -78,11 +76,10 @@ public class MenuController {
         root = FXMLLoader.load(getClass().getResource("AdminPage/Manual.fxml"));
         stage = (Stage)((Node)event.getSource()).getScene().getWindow();
         scene = new Scene(root);
+        this.generateManual(event);
+        System.out.println("Manual");
         stage.setScene(scene);
         stage.show();
-        this.clearVBox();
-        this.generateManual();
-        System.out.println("Manual");
     }
     @FXML
     public void changeToContact(MouseEvent event) throws IOException {
@@ -188,11 +185,81 @@ public class MenuController {
     /*
         todo : Manual
     */
+    @FXML
+    public void generateManual(MouseEvent event) {
+        vBoxManual.getChildren().clear();
+        final int big_font = 24;
+        final int small_font = 18;
+        Text titre = new Text("Manuel d'Utilisation - Système de Gestion des Plaques d'Immatriculation");
+        titre.setStyle("-fx-font-size: 30px; -fx-font-weight: bold; -fx-fill: Red;");
 
-    public void generateManual() {
-        Text text = new Text("This is the manual");
-        text.setStyle("-fx-font-size: 20px; -fx-font-weight: bold; -fx-fill: green;");
-        vBoxManual.getChildren().add(text);
+        Text Introduction = new Text("\n\nIntroduction : ");
+        Introduction.setStyle("-fx-font-size: " + big_font + "px; -fx-font-weight: bold; -fx-fill: black;");
+        Text introduction_text = new Text("\tBienvenue dans le système de gestion des plaques d'immatriculation. Ce manuel fournit des instructions détaillées sur " +
+                "\nl'utilisation de différentes fonctionnalités disponibles pour les administrateurs.");
+        introduction_text.setStyle("-fx-font-size: " + small_font + "px; -fx-font-weight: bold; -fx-fill: black;");
+
+        Text connexion = new Text("\nConnexion : ");
+        connexion.setStyle("-fx-font-size: " + big_font + "px; -fx-font-weight: bold; -fx-fill: black;");
+        Text connexion_text = new Text("\tPour vous connecter, veuillez entrer votre nom d'utilisateur et votre mot de passe dans les champs appropriés. " +
+                "\nCliquez sur le bouton de connexion pour accéder à votre compte.");
+        connexion_text.setStyle("-fx-font-size: " + small_font + "px; -fx-font-weight: bold; -fx-fill: black;");
+
+        Text tbl_de_bord = new Text("\nTableau de bord : ");
+        tbl_de_bord.setStyle("-fx-font-size: " + big_font + "px; -fx-font-weight: bold; -fx-fill: black;");
+        Text tbl_de_bord_text = new Text("\tLe tableau de bord principal présente plusieurs options pour la gestion des plaques d'immatriculation.");
+        tbl_de_bord_text.setStyle("-fx-font-size: " + small_font + "px; -fx-font-weight: bold; -fx-fill: black;");
+
+        Text nav = new Text("\nNavigation : ");
+        nav.setStyle("-fx-font-size: " + big_font + "px; -fx-font-weight: bold; -fx-fill: black;");
+        Text nav_text = new Text("\tPour naviguer entre les différentes pages, utilisez les boutons de navigation situés a gauche de l'écran. " +
+                "\nCliquez sur le bouton correspondant à la page que vous souhaitez visiter.");
+        nav_text.setStyle("-fx-font-size: " + small_font + "px; -fx-font-weight: bold; -fx-fill: black;");
+
+        Text pages = new Text("\nPages : ");
+        pages.setStyle("-fx-font-size: " + big_font + "px; -fx-font-weight: bold; -fx-fill: black;");
+        Text pages_text = new Text("\tLes pages disponibles sont les suivantes : \n" +
+                "\t\t- Accueil : Permet de revenir à la page d'accueil du tableau de bord.\n" +
+                "\t\t- Profil : Affiche et permet de modifier les informations du profil de l'administrateur.\n" +
+                "\t\t- Listes : Affiche la liste des utilisateurs enregistrés dans le système.\n" +
+                "\t\t- Statistiques : Affiche les statistiques et les rapports sur l'utilisation du système.\n" +
+                "\t\t- Manuel : Accède au manuel d'utilisation pour une référence rapide.\n" +
+                "\t\t- Contact : Pour contacter les developpeurs du systeme\n");
+        pages_text.setStyle("-fx-font-size: " + small_font + "px; -fx-font-weight: bold; -fx-fill: black;");
+
+        Text fct_princi = new Text("\nFonctionnalités Principales : ");
+        fct_princi.setStyle("-fx-font-size: " + big_font + "px; -fx-font-weight: bold; -fx-fill: black;");
+        Text fct_princi_text = new Text("\tLes fonctionnalités principales disponibles pour les administrateurs sont les suivantes : \n" +
+                "\t\t- Ajouter un utilisateur : Permet d'ajouter un nouvel utilisateur au système.\n" +
+                "\t\t- Rechercher un utilisateur : Permet de rechercher un utilisateur existant du système.\n" +
+                "\t\t- Supprimer un utilisateur : Permet de supprimer un utilisateur existant du système.\n" +
+                "\t\t- Modifier un utilisateur : Permet de modifier les informations d'un utilisateur existant.\n" +
+                "\t\t- Afficher les utilisateurs : Affiche la liste des utilisateurs enregistrés dans le système.\n" +
+                "\t\t- Afficher les statistiques : Affiche les statistiques et les rapports sur l'utilisation du système.\n");
+        fct_princi_text.setStyle("-fx-font-size: " + small_font + "px; -fx-font-weight: bold; -fx-fill: black;");
+
+
+        vBoxManual.getChildren().add(titre);
+        vBoxManual.getChildren().add(Introduction);
+        vBoxManual.getChildren().add(introduction_text);
+
+        vBoxManual.getChildren().add(connexion);
+        vBoxManual.getChildren().add(connexion_text);
+
+        vBoxManual.getChildren().add(tbl_de_bord);
+        vBoxManual.getChildren().add(tbl_de_bord_text);
+
+        vBoxManual.getChildren().add(nav);
+        vBoxManual.getChildren().add(nav_text);
+
+        vBoxManual.getChildren().add(pages);
+        vBoxManual.getChildren().add(pages_text);
+
+        vBoxManual.getChildren().add(fct_princi);
+        vBoxManual.getChildren().add(fct_princi_text);
+
         scrollPaneManual.setContent(vBoxManual);
     }
+
+
 }
